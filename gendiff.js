e9fab75438df0260dabc9a4c354055c15f45a2de
Egor Chebanov <egorchiba@gmail.com>
2023-06-28 18:18:02 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,8 @@ const { program } = require('commander');
 program
   .name('gendiff')
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
