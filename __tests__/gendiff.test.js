a0daf2b6db51ac8ef9ea524006d50fffd0ef413d
Egor Chebanov <egorchiba@gmail.com>
2023-06-30 12:21:55 +0300

    step 4 part 1

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,5 +3,5 @@ import genDiff from '/Users/egorchebanov/frontend-project-46/index.js';
 import { readFileSync } from 'node:fs';
 
 test('gendiff', () => {
-    expect(genDiff('/Users/egorchebanov/frontend-project-46/fixtures/file1.json', '/Users/egorchebanov/frontend-project-46/fixtures/file2.json')).toBe(readFileSync('/Users/egorchebanov/frontend-project-46/fixtures/result' ,'utf8'));
+    expect(genDiff('./fixtures/file1.json', './fixtures/file2.json')).toBe(readFileSync('./fixtures/result' ,'utf8'));
 })
