69a84cf8f3286bb4788416e0591593db0651c66f
Egor Chebanov <egorchiba@gmail.com>
2023-06-30 12:23:26 +0300

    step 4 part 1

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,4 @@
-import genDiff from '/Users/egorchebanov/frontend-project-46/index.js';
+import genDiff from '../index.js';
 // import { dirname } from 'path';
 import { readFileSync } from 'node:fs';
 
