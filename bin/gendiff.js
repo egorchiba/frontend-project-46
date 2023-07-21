cd84b1ca7384fd9bff42973fbdea5d8bb31471f4
Egor Chebanov <egorchiba@gmail.com>
2023-07-21 15:07:45 +0300

    step 8

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ import generateDiff from '../src/index.js';
 import { program } from 'commander';
 
 program
-  .name('genDiff')
+  .name('gendiff')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
