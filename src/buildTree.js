8aa83bc0a87f06490347088535acbb48c79a35f8
Egor Chebanov <egorchiba@gmail.com>
2023-07-21 16:20:29 +0300

    step 8

--- src/buildTree.js
+++ src/buildTree.js
@@ -4,20 +4,19 @@ const makeTree = (obj1, obj2) => {
   const keysObj = { ...obj1, ...obj2 };
   const keys = Object.keys(keysObj);
   const sortedKeys = _.sortBy(keys);
-  const diffTree = [];
-  sortedKeys.forEach((prop) => {
+  const diffTree = sortedKeys.map((prop) => {
     if ((typeof obj1[prop] === 'object') && (typeof obj2[prop] === 'object')) {
-      diffTree.push({ key: prop, status: 'nested', children: makeTree(obj1[prop], obj2[prop]) });
+      return { key: prop, status: 'nested', children: makeTree(obj1[prop], obj2[prop]) };
     } else if (obj2[prop] === undefined) {
-      diffTree.push({ key: prop, value: obj1[prop], status: 'removed' });
+      return { key: prop, value: obj1[prop], status: 'removed' };
     } else if (obj1[prop] === undefined) {
-      diffTree.push({ key: prop, value: obj2[prop], status: 'added' });
+      return { key: prop, value: obj2[prop], status: 'added' };
     } else if (obj1[prop] === obj2[prop]) {
-      diffTree.push({ key: prop, value: obj1[prop], status: 'unchanged' });
+      return { key: prop, value: obj1[prop], status: 'unchanged' };
     } else if (obj1[prop] !== obj2[prop]) {
-      diffTree.push({
+      return {
         key: prop, oldValue: obj1[prop], newValue: obj2[prop], status: 'updated',
-      });
+      };
     }
   });
 
