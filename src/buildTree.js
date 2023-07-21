2865c643b2e0edefafc8a559de84acbae7b253bf
Egor Chebanov <egorchiba@gmail.com>
2023-07-21 16:30:14 +0300

    step 8

--- src/buildTree.js
+++ src/buildTree.js
@@ -7,17 +7,19 @@ const makeTree = (obj1, obj2) => {
   const diffTree = sortedKeys.map((prop) => {
     if ((typeof obj1[prop] === 'object') && (typeof obj2[prop] === 'object')) {
       return { key: prop, status: 'nested', children: makeTree(obj1[prop], obj2[prop]) };
-    } else if (obj2[prop] === undefined) {
-      return { key: prop, value: obj1[prop], status: 'removed' };
-    } else if (obj1[prop] === undefined) {
-      return { key: prop, value: obj2[prop], status: 'added' };
-    } else if (obj1[prop] === obj2[prop]) {
-      return { key: prop, value: obj1[prop], status: 'unchanged' };
-    } else if (obj1[prop] !== obj2[prop]) {
-      return {
-        key: prop, oldValue: obj1[prop], newValue: obj2[prop], status: 'updated',
-      };
     }
+    if (obj2[prop] === undefined) {
+      return { key: prop, value: obj1[prop], status: 'removed' };
+    }
+    if (obj1[prop] === undefined) {
+      return { key: prop, value: obj2[prop], status: 'added' };
+    }
+    if (obj1[prop] === obj2[prop]) {
+      return { key: prop, value: obj1[prop], status: 'unchanged' };
+    }
+    return {
+      key: prop, oldValue: obj1[prop], newValue: obj2[prop], status: 'updated',
+    };
   });
 
   return diffTree;
